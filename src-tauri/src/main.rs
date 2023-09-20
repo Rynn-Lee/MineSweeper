604468c8dd8f3965ff97659dacdbb85ba8fa0adf
Rynn Lee <20amigo04@gmail.com>
2023-09-20 21:10:05 +0600

    Removed Rust functions

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,28 +5,6 @@ use serde_json::Number;
 
 fn main() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet, recursion])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
-}
-
-#[tauri::command]
-fn greet(msg: String) -> String {
-  let str = format!("Hewwo wowld! {}", msg);
-  str.into()
-  // let strvar = format!("Hewwo wowld!, {}", msg).into();
-}
-
-// #[tauri::command]
-// fn recursion(x: Number, y: Number, new_arr: &dyn Any){
-//   println!("Hewo")
-// }
-
-
-#[tauri::command]
-fn recursion(x: Number, y: Number, new_arr: String) -> String {
-  let str = format!("Hewwo wowld! {}", new_arr);
-  println!("{},{},{}", x, y, new_arr);
-  str.into()
-  // let strvar = format!("Hewwo wowld!, {}", msg).into();
 }
